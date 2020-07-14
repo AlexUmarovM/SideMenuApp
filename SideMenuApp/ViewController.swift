bd6bad6744590ad115cb43181f2d51759a405b41
Alex Umarov <umarov.iosdev@gmail.com>
2020-07-14 15:43:03 +0300

    Initial Commit

---
+++ SideMenuApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SideMenuApp
+//
+//  Created by Александр Умаров on 14.07.2020.
+//  Copyright © 2020 Александр Умаров. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
